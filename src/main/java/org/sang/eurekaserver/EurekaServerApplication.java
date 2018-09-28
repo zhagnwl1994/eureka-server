4a2e9d38d3a3a1a5e3ff2ff33a3719c591205ec5
zhangwl1994 <zwlwangy163@163.com>
2018-09-28 22:47:44 +0800

    create eureka_server

--- src/main/java/org/sang/eurekaserver/EurekaServerApplication.java
+++ src/main/java/org/sang/eurekaserver/EurekaServerApplication.java
@@ -2,7 +2,9 @@ package org.sang.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//启动一个服务注册中心 @EnableEurekaServer
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
