3192b34e118546f3830a493a6acd1bc19af148df
zhagnwl1994 <43334427+zhagnwl1994@users.noreply.github.com>
2018-09-28 22:14:06 +0800

    create eureka_server

---
+++ src/main/java/org/sang/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,12 @@
+package org.sang.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
